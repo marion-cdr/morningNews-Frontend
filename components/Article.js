84d4d80db25e7985373febdb06a5f74739ddcb40
marion-cdr <107422165+marion-cdr@users.noreply.github.com>
2024-04-17 10:54:31 +0200

    Update Article.js

--- components/Article.js
+++ components/Article.js
@@ -17,7 +17,7 @@ function Article(props) {
 			return;
 		}
 
-		fetch(`https://morning-news-backend-beta.vercel.app/users/canBookmark/${user.token}`)
+		fetch(`https://morning-news-backend-mocha.vercel.app/users/canBookmark/${user.token}`)
 			.then(response => response.json())
 			.then(data => {
 				if (data.result && data.canBookmark) {
